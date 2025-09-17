2c4bbbaeaaa134ffa86e9963c031d71d7d2955f1
Cedric Govaerts <cdgovaerts@gmail.com>
2025-09-18 01:33:35 +0200

    cocktail page html

--- src/cocktailPage.jsx
+++ src/cocktailPage.jsx
@@ -1,6 +1,26 @@
 function CocktailPage(){
     return(
-        <div>Hello</div>
+        <div>
+            <header>
+                <h1>MOJITO</h1>
+                <button>New search</button>
+            </header>
+            <main>
+                <div>
+                    <ul>
+                        <li>2-3 oz Light rum</li>
+                        <li>Juice of 1 Lime</li>
+                        <li>2 tsp Sugar</li>
+                        <li>2-4 Mint</li>
+                        <li>Soda water</li>
+                    </ul>
+                    <p>Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water. Garnish and serve with straw.</p>
+                </div>
+                <div>
+                    <img src="./assets/img/mojito.png" alt="cocktail image" />
+                </div>
+            </main>
+        </div>
     )
 }
 
