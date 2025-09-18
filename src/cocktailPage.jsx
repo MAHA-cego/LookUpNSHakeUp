be3436644544fe12a6cf86e372650b52eb7a07eb
Cedric Govaerts <cdgovaerts@gmail.com>
2025-09-18 12:29:07 +0200

    cocktailPage

--- src/cocktailPage.jsx
+++ src/cocktailPage.jsx
@@ -1,7 +1,9 @@
+import mojitoImg from './assets/img/mojito.png';
+
 function CocktailPage(){
     return(
-            <main>
-                <div>
+            <main className="grid grid-cols-[4fr_1fr_7fr] h-[calc(100vh-100px)] overflow-hidden">
+                <div className="flex flex-col gap-5 col-start-1 col-span-1 ml-5 mb-9 justify-end font-[CabinetGrotesk] text-xl tracking-wider">
                     <ul>
                         <li>2-3 oz Light rum</li>
                         <li>Juice of 1 Lime</li>
@@ -11,8 +13,8 @@ function CocktailPage(){
                     </ul>
                     <p>Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water. Garnish and serve with straw.</p>
                 </div>
-                <div>
-                    <img src="./assets/img/mojito.png" alt="cocktail image" />
+                <div className="col-start-3 h-full overflow-hidden">
+                    <img src={mojitoImg} alt="cocktail image" className="w-full h-auto max-h-[calc(100vh-100px)] object-cover"/>
                 </div>
             </main>
     )
